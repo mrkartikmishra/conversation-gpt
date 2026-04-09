54569ba113eb90afc2c9fb3f567a5ed1694ef788
Kartik Mishra <kartikmishra.km44@gmail.com>
2026-04-10 00:12:10 +0530

    Add multi model supprt

--- app/api/chat/graph.ts
+++ app/api/chat/graph.ts
@@ -2,9 +2,11 @@ import { END, GraphNode, START, StateGraph } from "@langchain/langgraph";
 
 import { MessagesState } from "./state";
 import { SystemMessage } from "@langchain/core/messages";
-import { llmModel } from "./model";
+import { getDynamicModel } from "./model";
 
 export const llmCall: GraphNode<typeof MessagesState> = async (state) => {
+  const llmModel = getDynamicModel("gemini-2.5-flash");
+
   const response = await llmModel.invoke([
     new SystemMessage("You are a helpful assistant."),
     ...state.messages,
